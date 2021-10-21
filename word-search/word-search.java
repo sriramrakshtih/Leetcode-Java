51f53d51e12e1e4682f544915c2e0292dc49082d
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-10-21 10:49:28 -0700

    Time: 239 ms (16.23%), Space: 38.7 MB (36.40%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -1,8 +1,8 @@
 class Solution {
-    int n, m; int [][] dirs;
+    int m; int n; int [][] dirs;
     public boolean exist(char[][] board, String word) {
         m = board.length; n = board[0].length;
-        dirs = new int [][] {{0,1},{0,-1}, {1,0}, {-1, 0}};
+        dirs = new int [][]{{0,1}, {0,-1}, {1,0},{-1,0}};
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++) {
                 if(dfs(board, word, i, j, 0)) return true;
@@ -11,24 +11,22 @@ class Solution {
         return false;
     }
     
-    private boolean dfs(char[][] board, String word, int i, int j, int index){
-        StringBuilder sb = new StringBuilder();
+    private boolean dfs(char [][] board, String word, int i, int j, int index) {
         //base case
         if(index == word.length()) return true;
         if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '#') return false;
         
         //logic
-        if(board[i][j] == word.charAt(index)){
+        if(word.charAt(index) == board[i][j]){
             char temp = board[i][j];
+            board[i][j] = '#';
             for(int [] dir : dirs){
-                board[i][j] = '#';
-                int row = i + dir[0];
-                int col = j + dir[1];
-                if(dfs(board, word, row, col, index+1)) return true;
+                int row = dir[0] + i;
+                int col = dir[1] + j;
+                if(dfs(board, word, row, col, index + 1)) return true;
             }
             board[i][j] = temp;
         }
-        
         return false;
     }
 }
