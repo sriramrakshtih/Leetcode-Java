385805e5616a2dccd05d0d5a692b59fc315f23ce
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-10-21 16:41:50 -0700

    Time: 92 ms (37.90%), Space: 37.9 MB (27.06%) - LeetHub

---
+++ beautiful-arrangement/beautiful-arrangement.java
@@ -0,0 +1,22 @@
+class Solution {
+    int count = 0;
+    public int countArrangement(int n) {
+        boolean [] visited = new boolean [n+1];
+        calc(n, 1, visited);
+        return count;
+    }
+    
+    private void calc(int n, int idx, boolean [] visited) {
+        if(idx > n){
+            count++;
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            if(!visited[i] && (idx % i == 0 || i % idx == 0)){
+                visited[i] = true;
+                calc(n, idx+1, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
