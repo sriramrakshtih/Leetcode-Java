a90b56c17b9616e9e7193c45bc8fce83f6379dff
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-10-22 22:37:42 -0700

    Time: 15 ms (28.18%), Space: 62.9 MB (7.52%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.java
+++ find-k-closest-elements/find-k-closest-elements.java
@@ -1,44 +1,29 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < k; i++) {
+            //System.out.println(arr[i]);
+            q.add(arr[i]);
+        }
+        
+        for(int i = k; i < arr.length; i++) {
+            //System.out.println(arr[i]);
+            if(x - q.peek() > arr[i] - x){
+                q.poll();
+                q.add(arr[i]);
+            } 
+            else {
+                break;
+            }
+        }
+        
+        
+        
         List<Integer> res = new ArrayList<>();
-        int low = 0; int high = arr.length - 1;
-        int k_idx = bs(arr, low, high, x);
-        System.out.print(k_idx);
-        int j = k_idx + 1;
-        
-        while(k_idx >= 0 && j < arr.length && res.size() != k){
-            if(Math.abs(arr[k_idx] - x) <= Math.abs(arr[j] - x)){
-                res.add(0, arr[k_idx]);
-                k_idx--;
-            } else {
-                res.add(arr[j]);
-                j++;
-            }
+        while(!q.isEmpty()) {
+            res.add(q.poll());
         }
         
-        while(res.size() < k){
-            if(k_idx >= 0){
-                res.add(0, arr[k_idx]);
-                k_idx--;
-            } else if(j < arr.length){
-                res.add(arr[j]);
-                j++;
-            }
-        }
         return res;
-    }
-    
-    private int bs(int [] arr, int low, int high, int k) {
-        while(low < high){
-            int mid = low + (high - low) / 2; 
-            if(arr[mid] == k || (arr[mid] < k && arr[mid + 1] > k)) {
-                return mid;
-            } else if(arr[mid] > k){
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
     }
 }
