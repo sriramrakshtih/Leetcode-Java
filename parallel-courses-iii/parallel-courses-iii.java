3210f079ddf4f06f30508ed8ecc0281200e599d1
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-10-26 12:42:19 -0700

    Time: 57 ms (48.77%), Space: 68 MB (84.56%) - LeetHub

--- parallel-courses-iii/parallel-courses-iii.java
+++ parallel-courses-iii/parallel-courses-iii.java
@@ -1,6 +1,5 @@
 class Solution {
-    
-    public class Pair {
+    class Pair {
         int first;
         int second;
         
@@ -9,67 +8,60 @@ class Solution {
             this.second = second;
         }
     }
-    
-    
     public int minimumTime(int n, int[][] relations, int[] time) {
-        
-        int [] indegree = new int[n+1];
         HashMap<Integer, List<Integer>> map = new HashMap<>();
-        Arrays.fill(indegree, 0);
+        int [] indegree = new int [n+1];
         
         for(int i = 0; i < relations.length; i++){
             indegree[relations[i][1]]++;
         }
         
+        
         for(int i = 0; i < relations.length; i++) {
-            
-            if(!map.containsKey(relations[i][1])){
-                map.put(relations[i][1], new ArrayList<>());
-            }
             if(!map.containsKey(relations[i][0])){
-                map.put(relations[i][0], new ArrayList<>());
+                 map.put(relations[i][0], new ArrayList<>());
             }
+            if(!map.containsKey(relations[i][1])){
+                 map.put(relations[i][1], new ArrayList<>());
+            }
+            
             map.get(relations[i][0]).add(relations[i][1]);
         }
         
-        // System.out.print(map);
-        //System.out.print(Arrays.toString(indegree));
-        
-        Queue<Pair> q = new LinkedList<>();
+        //System.out.print(map);
         HashMap<Integer, Integer> map2 = new HashMap<>();
-        for(int i = 1; i <= n; i++){
+        
+        for(int i = 1; i<=n; i++) {
             map2.put(i, 0);
         }
         
-        
-        for(int i = 1, j = 0; i < indegree.length; i++,j++){
+        Queue<Pair> q = new LinkedList<>();
+        // System.out.print(Arrays.toString(indegree));
+        for(int i = 1; i <= n; i++) {
             if(indegree[i] == 0){
-                Pair p = new Pair(i, time[j]);
+                Pair p = new Pair(i, time[i-1]);
+                // System.out.println(p.first);
+                // System.out.println(p.second);
                 q.add(p);
             }
         }
         
-        List<Integer> li = new ArrayList<>();
+        int max = 0;
         
         while(!q.isEmpty()){
             Pair p = q.poll();
-            li.add(p.second);
+            max = Math.max(max, p.second);
+            
             List<Integer> children = map.getOrDefault(p.first, new ArrayList<>());
             
-            for(int child : children){
-                map2.put(child, Math.max(map2.get(child),p.second));
+            for(int child : children) {
+                map2.put(child, Math.max(map2.get(child), p.second));
                 indegree[child]--;
-                if(indegree[child] == 0){
-                    Pair pq = new Pair(child, map2.get(child)+time[child-1]);
+                if(indegree[child] == 0) {
+                    Pair pq = new Pair(child, map2.get(child)+time[child - 1]);
                     q.add(pq);
                 }
             }
-            
-        }
-        int max = 0;
-        for(int i = 0; i < li.size(); i++) {
-            //System.out.println(li.get(i));
-            max = Math.max(max, li.get(i));
         }
         return max;
     }
