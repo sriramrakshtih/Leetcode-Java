560d5b66761b474ff5034c1b74a8782ce3cfec44
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-10-29 14:55:30 -0700

    Time: 6 ms (57.36%), Space: 39.4 MB (83.29%) - LeetHub

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -1,46 +1,47 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int [] indegree = new int[numCourses];
         HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int [] indegree = new int [numCourses];
+        
         
         for(int i = 0; i < prerequisites.length; i++) {
             indegree[prerequisites[i][0]]++;
-            if(!map.containsKey(prerequisites[i][1])){
-                map.put(prerequisites[i][1], new ArrayList<>());
-            }
             if(!map.containsKey(prerequisites[i][0])){
                 map.put(prerequisites[i][0], new ArrayList<>());
+            }
+            if(!map.containsKey(prerequisites[i][1])){
+                map.put(prerequisites[i][1], new ArrayList<>());
             }
             map.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
         
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < indegree.length; i++){
-            if(indegree[i] == 0){
-                q.add(i);
+        for(int i = 0 ; i < indegree.length; i++) {
+            if(indegree[i] == 0) {
+                q.add(i);    
             }
         }
         
         while(!q.isEmpty()){
             int curr = q.poll();
-            List<Integer> li = map.get(curr);
-            if(li != null) {
-               for(int child: li) {
-                  // System.out.println(child + "  " + indegree[child]);
+            List<Integer> list = map.get(curr);
+            if(list != null){
+                for(int child : list){
                     indegree[child]--;
                     if(indegree[child] == 0){
                         q.add(child);
                     }
-                } 
+                }
             }
+            
         }
         
         for(int i = 0; i < indegree.length; i++){
             if(indegree[i] != 0){
-            //    System.out.println(i + " " + indegree[i]);
                 return false;
             }
         }
+        //System.out.print(Arrays.toString(indegree));
         return true;
     }
 }
