f3d5448f71cfb4ad08c103d7b910e2c54e09e1e2
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-04 19:21:05 -0700

    Time: 4 ms (92.12%), Space: 38.9 MB (82.68%) - LeetHub

--- edit-distance/edit-distance.java
+++ edit-distance/edit-distance.java
@@ -1,24 +1,21 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word2.length();
-        int n = word1.length();
-        int [][] dp = new int[m+1][n+1];
+        int m = word2.length(); int n = word1.length();
+        int dp [][] = new int[m+1][n+1];
         
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++){
             dp[0][i] = i;
         }
-        
-        for(int i = 1; i <= m; i++) {
+        for(int i = 1; i <= m; i++){
             dp[i][0] = i;
         }
         
-        // System.out.print(Arrays.deepToString(dp));
-        for(int i = 1; i <= m; i++) {
+        for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 if(word2.charAt(i-1) == word1.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
                 } else {
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
                 }
             }
         }
