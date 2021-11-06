ac1ab7606b169f151c4339960be84814d2c9ba2e
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-06 14:23:25 -0700

    Time: 238 ms (38.89%), Space: 39.8 MB (86.21%) - LeetHub

--- russian-doll-envelopes/russian-doll-envelopes.java
+++ russian-doll-envelopes/russian-doll-envelopes.java
@@ -4,11 +4,10 @@ class Solution {
         int max = 1;
         Arrays.sort(envelopes, (a,b) -> a[0]-b[0]);
         Arrays.fill(dp, 1);
-        
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 0; j < i; j++){
+        for(int i = 0; i < envelopes.length; i++){
+            for(int j = 0; j < i; j++) {
                 if(envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]){
-                    dp[i] = Math.max(dp[i], dp[j] +1);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                     max = Math.max(max, dp[i]);
                 }
             }
