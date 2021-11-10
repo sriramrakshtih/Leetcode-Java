617ec3fef94aa442f847a8e20cd868fe22f77533
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-10 14:45:43 -0800

    Time: 22 ms (51.16%), Space: 38.8 MB (98.17%) - LeetHub

--- number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
+++ number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
@@ -1,42 +1,34 @@
 class Solution {
     public int numSplits(String s) {
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        int goodSplits = 0;
+        int [] left = new int [26];
+        int [] right = new int [26];
         
-        map1.put(s.charAt(0), 1);
-        for(int i = 1; i < s.length(); i++) {
-            if(!map2.containsKey(s.charAt(i))){
-                map2.put(s.charAt(i), 0);
-            }
-            map2.put(s.charAt(i), map2.get(s.charAt(i)) + 1);
+        for(int i = 0 ;i < s.length(); i++) {
+            char c = s.charAt(i);
+            right[c - 'a']++;
         }
         
-        System.out.print(map1);
-        System.out.print(map2);
-        
-        
-        int count = 0;
-        for(int i = 1; i < s.length(); i++) {
-            if(map1.size() == map2.size()){
-                count++;
-            }
+        for(int i = 0 ;i < s.length(); i++) {
+            char c = s.charAt(i);
+            left[c - 'a']++;
+            right[c - 'a']--;
             
+            int leftSp = good(left);
+            int rightSp = good(right);
             
-            if(map1.containsKey(s.charAt(i))){
-                map2.put(s.charAt(i), map2.get(s.charAt(i)) - 1);
-                map1.put(s.charAt(i), map1.get(s.charAt(i)) + 1);
-            } else {
-                map1.put(s.charAt(i), 0);
-                map1.put(s.charAt(i), map1.get(s.charAt(i)) + 1);
-                map2.put(s.charAt(i), map2.get(s.charAt(i)) - 1);
-                
-            }
-            
-            if(map2.get(s.charAt(i)) == 0){
-                map2.remove(s.charAt(i));
-            }
+            if(leftSp == rightSp) goodSplits++;
         }
         
-        return count;
+        return goodSplits;
+    }
+    
+    private int good(int [] count) {
+        int c = 0;
+        for(int i : count) {
+            if(i != 0)
+                c++;
+        }
+        return c;
     }
 }
