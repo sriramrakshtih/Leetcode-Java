8dd1b2524e58f421b4b62770aba8bb0a77f01f6c
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-15 17:00:47 -0800

    Time: 19 ms (55.86%), Space: 40.2 MB (46.59%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        HashSet<Integer> set = new HashSet<>();
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i< s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '('){
+                st.push(i);
+            } if(c == ')'){
+                if(st.isEmpty()){
+                    set.add(i);
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        
+        while(!st.isEmpty()) {
+            set.add(st.pop());
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(!set.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
