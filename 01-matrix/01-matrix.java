058241da4f8644e00960ed7d7405e1c400de1a90
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-18 13:50:36 -0800

    Time: 13 ms (64.69%), Space: 43 MB (41.61%) - LeetHub

--- 01-matrix/01-matrix.java
+++ 01-matrix/01-matrix.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int [][] dirs = new int [][]{{0,-1}, {0,1}, {1,0}, {-1, 0}};
-        int [][] newMat = new int [mat.length][mat[0].length];
-        Queue<int []> q = new LinkedList<>();
-        for(int i = 0; i < mat.length; i++){
+        Queue<int[]> q = new LinkedList<>();
+        int [][] newMat = new int[mat.length][mat[0].length];
+        
+        for(int i = 0; i < mat.length; i++) {
             for(int j = 0; j < mat[0].length; j++) {
-                if(mat[i][j] == 0){
-                    q.offer(new int[]{i,j});
+                if(mat[i][j] == 0) {
+                    q.offer(new int []{i,j});
                 } else {
                     newMat[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
         
-        for (int [] s : dirs) System.out.println(Arrays.toString(s));
+        int [][] dirs = new int [][] {{0,1}, {0,-1}, {1,0}, {-1,0}};
         
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             int [] curr = q.poll();
             
-            for(int [] dir : dirs){
-                int newX = curr[0] + dir[0];
-                int newY = curr[1] + dir[1];
+            for(int[] dir : dirs) {
+                int row = curr[0] + dir[0];
+                int col = curr[1] + dir[1];
                 
-                if(newX >= 0 && newX < mat.length && newY >= 0 && newY < mat[0].length){
-                    if(newMat[curr[0]][curr[1]]  +1 < newMat[newX][newY]){
-                        newMat[newX][newY] = newMat[curr[0]][curr[1]] + 1;
-                        q.offer(new int[]{newX, newY});
+                if(row >= 0 && row < newMat.length && col >= 0 && col < newMat[0].length) {
+                    if(newMat[curr[0]][curr[1]] + 1 < newMat[row][col]){
+                        newMat[row][col] = newMat[curr[0]][curr[1]] + 1;
+                        q.offer(new int[]{row, col});
                     }
                 }
             }
