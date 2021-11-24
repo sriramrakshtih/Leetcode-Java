5300cf3b36d280c23d20cb2d7c7803a4fc49b5eb
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-23 16:01:53 -0800

    Time: 13 ms (50.79%), Space: 38.8 MB (96.63%) - LeetHub

--- minimum-window-substring/minimum-window-substring.java
+++ minimum-window-substring/minimum-window-substring.java
@@ -1,43 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(t.length() > s.length()) return "";
-        int begin = 0; int end = 0; int head = 0;
+        int begin = 0; int end = 0, head = 0;
         int len = Integer.MAX_VALUE;
-        String res = ""; 
-        
         HashMap<Character, Integer> map = new HashMap<>();
-        
-        for(char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c,0) + 1);
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
+        
         int counter = map.size();
         while(end < s.length()) {
             char endC = s.charAt(end);
-            
             if(map.containsKey(endC)){
                 map.put(endC, map.get(endC) - 1);
-                if(map.get(endC) == 0) counter--;
+                if(map.get(endC) == 0){
+                    counter--;
+                }
             }
             end++;
-            //System.out.println("Begin " + begin + " end " + end);
-            
             while(counter == 0) {
-                
                 char beginC = s.charAt(begin);
                 if(map.containsKey(beginC)){
                     map.put(beginC, map.get(beginC) + 1);
-                    if(map.get(beginC) > 0) counter++;
+                    if(map.get(beginC) > 0){
+                        counter++;
+                    }
                 }
                 
-                if(end - begin < len){
+                if(len > end-begin){
                     len = end - begin;
                     head = begin;
-                    
                 }
                 begin++;
             }
         }
+        
         if(len == Integer.MAX_VALUE) return "";
-        return s.substring(head, head + len);
+        return s.substring(head, head+len);
     }
 }
