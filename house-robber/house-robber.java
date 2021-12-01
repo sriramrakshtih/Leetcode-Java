074666bc269069c13edf23e9d7e6825924efc706
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-30 16:39:53 -0800

    Time: 0 ms (100.00%), Space: 36 MB (99.43%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,15 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int loot = 0; int skip = 0;
+        int skip = 0; int loot = 0;
         for(int i = 0; i < nums.length; i++) {
             int temp = skip;
             
-            //don't choose
             skip = Math.max(temp, loot);
-            
             loot = nums[i] + temp;
         }
-        
-        return Math.max(skip, loot);
+        return Math.max(loot, skip);
     }
 }
