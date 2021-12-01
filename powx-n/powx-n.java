2910492cb02135ed8c11f185b0ab9cc164d2a712
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-30 18:59:56 -0800

    Time: 0 ms (100.00%), Space: 38.1 MB (90.10%) - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,16 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n == 0){
-            return 1;
-        }
+        if(n == 0)return 1;
+        
         double temp = myPow(x, n/2);
-        if(n % 2 == 0){
-            temp = temp * temp;
+        
+        if(n % 2 == 0) {
+            temp = (temp * temp);
         } else {
-            if(n >= 0){
+            if(n >= 0) {
                 temp = (temp * temp) * x;
             } else {
-                temp = (temp * temp) * 1/x; 
+                temp = (temp * temp) * 1/x;
             }
         }
         return temp;
