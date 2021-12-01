5728bd2c0f54d46dfdfb1f4de5c2acc9e04c174e
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-30 22:13:56 -0800

    Time: 7 ms (97.99%), Space: 41.8 MB (96.21%) - LeetHub

--- capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,34 +1,39 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int left = Integer.MIN_VALUE;
-        int right = 0;
+        int min = Integer.MAX_VALUE;
+        int low = 1;
+        int high = 0;
+        
         for(int weight : weights) {
-            left = Math.max(left, weight);
-            right += weight;
+            high += weight;
         }
         
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(isValid(weights, mid) <= days){
-                right = mid;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(isValid(weights, mid) <= days) {
+                high = mid - 1;
+                min = Math.min(mid, min);
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
         
-        return left;
+        return min;
     }
     
     private int isValid(int [] weights, int capacity) {
-        int count = 1;
+        int days = 1;
         int total = 0;
         for(int i = 0; i < weights.length; i++) {
+            if(weights[i] > capacity) return Integer.MAX_VALUE;
             total += weights[i];
             if(total > capacity) {
-                count++;
+                days++;
                 total = weights[i];
             }
         }
-        return count;
+        
+        return days;
     }
 }
