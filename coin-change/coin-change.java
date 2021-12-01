727d45b9148c819488f2d1b7c6c5b80a935b86a1
Sriram Rakshith Kolar <sriramrakshith@gmail.com>
2021-11-30 17:18:01 -0800

    Time: 21 ms (52.68%), Space: 39 MB (50.13%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,32 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
+        int [][] dp = new int [coins.length + 1][amount+1];
         
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        
-        for(int i = 0; i <= coins.length; i++) {
-            dp[i][0] = 0;
+        for(int i = 1; i < dp[0].length; i++) {
+            dp[0][i] = 9999;
         }
         
-        for(int j = 1; j <= amount; j++) {
-            dp[0][j] = 9999;
-        }
-        
-        for(int i = 1; i <= coins.length; i++){
-            for(int j = 1; j <= amount; j++){
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++){
                 if(j < coins[i-1]){
                     dp[i][j] = dp[i-1][j];
                 } else {
-                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j - coins[i-1]] + 1);
+                    dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j - coins[i-1]]);
                 }
             }
         }
-        
-        int result = dp[coins.length][amount];
-
-       if(result >= 9999) return -1;
-
-       else return result;
-        //return dp[coins.length][amount];
+        int result = dp[dp.length-1][dp[0].length-1];
+        if(result >= 9999) return -1;
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
